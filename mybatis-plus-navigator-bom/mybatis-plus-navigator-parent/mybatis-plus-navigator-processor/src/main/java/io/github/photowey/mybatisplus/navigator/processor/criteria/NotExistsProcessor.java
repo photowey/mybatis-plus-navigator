39d8d4dc5f573afbdabd34b72c593b4333bffd29
photowey <photowey@gmail.com>
2024-04-10 22:25:17 +0800

    feat: Add: 1.Like/NotLike, Exists/NotExists AnnotationProcessor.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ mybatis-plus-navigator-bom/mybatis-plus-navigator-parent/mybatis-plus-navigator-processor/src/main/java/io/github/photowey/mybatisplus/navigator/processor/criteria/NotExistsProcessor.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.mybatisplus.navigator.processor.criteria;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.github.photowey.mybatisplus.navigator.annotation.query.NotExists;
+import io.github.photowey.mybatisplus.navigator.processor.annotation.component.criteria.CriteriaProcessor;
+import io.github.photowey.mybatisplus.navigator.processor.model.query.AbstractQuery;
+
+import java.lang.reflect.Field;
+
+/**
+ * {@code NotExistsProcessor}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/04/10
+ */
+@CriteriaProcessor(criteria = NotExists.class)
+public class NotExistsProcessor<QUERY extends AbstractQuery<ENTITY>, ENTITY>
+        extends AbstractCriteriaAnnotationProcessorAdaptor<NotExists, QUERY, QueryWrapper<ENTITY>, ENTITY> {
+
+    @Override
+    public boolean process(QueryWrapper<ENTITY> queryWrapper, Field field, QUERY query, NotExists annotation) {
+        final Object value = this.tryExtractFiledValue(field, query);
+        if (this.isEmpty(value)) {
+            return true;
+        }
+        String existsSql = annotation.existsSql();
+        if (this.isNotEmpty(existsSql)) {
+            queryWrapper.notExists(existsSql, value);
+        }
+
+        return true;
+    }
+}
