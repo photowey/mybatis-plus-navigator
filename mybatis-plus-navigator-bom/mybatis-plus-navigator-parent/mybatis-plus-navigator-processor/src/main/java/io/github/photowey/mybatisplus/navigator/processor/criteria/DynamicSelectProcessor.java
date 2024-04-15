7f6c43aefdcfcf396c96ae3acd4585274e8eb3e0
photowey <photowey@gmail.com>
2024-04-15 23:44:08 +0800

    feat: Add: 1.DynamicSelect/Select AnnotationProcessor.
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ mybatis-plus-navigator-bom/mybatis-plus-navigator-parent/mybatis-plus-navigator-processor/src/main/java/io/github/photowey/mybatisplus/navigator/processor/criteria/DynamicSelectProcessor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.photowey.mybatisplus.navigator.processor.criteria;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.github.photowey.mybatisplus.navigator.annotation.query.DynamicSelect;
+import io.github.photowey.mybatisplus.navigator.processor.annotation.component.criteria.CriteriaProcessor;
+import io.github.photowey.mybatisplus.navigator.processor.model.query.AbstractQuery;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * {@code DynamicSelectProcessor}
+ *
+ * @author photowey
+ * @version 1.0.0
+ * @since 2024/04/15
+ */
+@CriteriaProcessor(criteria = DynamicSelect.class)
+public class DynamicSelectProcessor<QUERY extends AbstractQuery<ENTITY>, ENTITY>
+        extends AbstractCriteriaAnnotationProcessorAdaptor<DynamicSelect, QUERY, QueryWrapper<ENTITY>, ENTITY> {
+
+    @Override
+    public boolean process(QueryWrapper<ENTITY> queryWrapper, Field field, QUERY query, DynamicSelect annotation) {
+        final List<String> values = this.tryExtractFiledListValues(field, query);
+        if (this.isEmpty(values)) {
+            return true;
+        }
+
+        queryWrapper.select(values);
+
+        return true;
+    }
+}
